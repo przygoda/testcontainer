90c2620ea735ccfa7ab92abdac6cd4914bfecb73
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-09-09 10:51:40 +0000

    GET / 2. Iteration

--- index.js
+++ index.js
@@ -13,12 +13,28 @@ app.listen(
 );
 
 // Endpoint GET
+// app.get('/' , (req,res) => {
+//     res.status(200).send({
+//         product: "T-Shirt",
+//         size: "L",
+//         prize: 10.00,
+//         hasLogo: false
+//     })
+// });
+
 app.get('/' , (req,res) => {
-    res.status(200).send({
-        product: "T-Shirt",
-        size: "L",
-        prize: 10.00,
-        hasLogo: false
-    })
+    res.status(200).send(`
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meine Seite</title>
+</head>
+<body>
+    <h1>Cool</h1>
+    <p>Das ist eine coole Sache!</p>
+</body>
+</html>`)
 });
 
